a214267c981187e2df7cb06c75de9bcb94f4db96
GC1C _10_トミグチ_カオル <k024g1003@g.neec.ac.jp>
2025-05-09 14:31:53 +0900

    PG3 1-2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int Recursive1(int kyuuryou, int zikan, int ippan) {
+	if (kyuuryou >= ippan) {
+		printf("%dŠÔŒã‚É’´‚¦‚é", zikan);
+		return zikan;
+	}
+	printf("%d@%d\n", kyuuryou, ippan);
+	kyuuryou += kyuuryou *2 - 50;
+
+	return (Recursive1(kyuuryou, ++zikan,ippan *2));
+}
+
 int main() {
 
-	printf("Hello PG3 00_01");
+	int saiki = 100;
+	int ippan = 1072;
+	int zikan = 1;
+	int resullt1 = Recursive1(saiki, zikan, ippan);;
 
 	return 0;
 }
